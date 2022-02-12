5bccc2c839fdd6bba43d59cd448de44a348314e6
atrox3d <atrox3d@gmail.com>
2022-02-12 17:11:16 +0100

    get video files, 1st version

--- main.sh
+++ main.sh
@@ -4,3 +4,19 @@
 info "START"
 #log wrong x
 
+extensions=(mp4 mkv)
+for ext in ${extensions[@]}
+do
+	_videos=(*.${ext})
+	videos=(${videos[@]} ${_videos[@]})
+	info ${videos[@]}
+done
+
+total_videos=${#videos[@]}
+info "total videos: $total_videos"
+if [ $total_videos -gt 1 ]
+then
+	fatal "too much video files"
+	exit
+fi
+
