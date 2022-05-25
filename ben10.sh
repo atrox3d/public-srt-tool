b6c1e155716c3a6b0495fc7489fd044fd08e9022
atrox3d <atrox3d@gmail.com>
2022-05-25 06:04:43 +0200

    add comments and examples

--- ben10.sh
+++ ben10.sh
@@ -13,23 +13,48 @@ HERE="$(dirname ${BASH_SOURCE[0]})"
 # save this script name
 NAME="$(basename ${BASH_SOURCE[0]})"
 
+# check params
 info "PARAMS | ${@}"
 if [ ${#} -gt 0 ]
 then
+	#
+	# loop through seasons
+	#
+	# ${1}/ : /e/VIDEO/serieTV ENG/ben 10/ 
+	# */    : 01/ 
+	# */    : Ben.10.S01.1080p.WEBRip.x265-RARBG/
+	#
 	for season in "${1}"/*/*/
 	do
-		info "pushd ${season}"
+		info "pushd season | ${season}"
 		pushd "${season}" &> /dev/null
-		
-			# ls -l "${season}"/Subs/*/
+			#
+			# loop through subtitles subdirs
+			# ${season}/ : /e/VIDEO/serieTV ENG/ben 10/01/Ben.10.S01.1080p.WEBRip.x265-RARBG/
+			# Subs/      : Subs/
+			# */         : Ben.10.S01E01.1080p.WEBRip.x265-RARBG/
+			#
 			for subsdir in "${season}"/Subs/*/
 			do
 				info "subsdir | ${subsdir}"
+				#
+				# loop through srtfiles
+				# ${subsdir} : /e/VIDEO/serieTV ENG/ben 10/01/Ben.10.S01.1080p.WEBRip.x265-RARBG//Subs/Ben.10.S01E01.1080p.WEBRip.x265-RARBG//
+				# */         : 2_English.srt
+				#
+				# TODO: stop at first or add counter
+				#
 				for srtfile in "${subsdir}"/*
 				do
 					info "srtfile | ${srtfile}"
+					#
+					# Ben.10.S01E01.1080p.WEBRip.x265-RARBG.srt
+					#
 					filename="$(basename "${subsdir}").srt"
 					info "filename | ${filename}"
+					#
+					# copy srt files at video files level
+					#
 					info "cp ${srtfile} ${season}/${filename}"
 					cp "${srtfile}" "${season}/${filename}" || {
 						fatal "error copying"
@@ -37,6 +62,7 @@ then
 					}
 				done
 			done
+		# cd back
 		info popd
 		popd  &> /dev/null
 	done
