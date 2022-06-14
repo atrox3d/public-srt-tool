992523c249913bb425ff334a9457fa974a6f0ac0
atrox3d <atrox3d@gmail.com>
2022-06-14 14:57:55 +0200

    boilerplate.sh

---
+++ boilerplate.sh
@@ -0,0 +1,21 @@
+# get current path
+HERE="$(dirname ${BASH_SOURCE[0]})"
+echo "HERE | ${HERE}"
+#
+# source logger
+#
+{
+	. "${HERE}/logger.include" || {
+		. "${HERE}/../logger.include" || {
+			echo "FATAL | cannot find logger.include"
+			exit 255
+		}
+	}
+} 2> /dev/null
+#
+#	setup variables and log level
+#
+HERE="$(dirname ${BASH_SOURCE[0]})"
+NAME="$(basename ${BASH_SOURCE[0]})"	# save this script name
+logger_setlevel info
+
